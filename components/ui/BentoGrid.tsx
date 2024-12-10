667d2840058d2ab30d41a3b4184db51f22f0d0d8
udutvic <udutvic@gmail.com>
2024-12-10 22:08:15 +0100

    Auto commit at 2024-12-10T21:08:15.798Z

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -10,7 +10,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-7 max-w-7xl mx-auto relative",
+        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-7 max-w-7xl mx-auto relative mb-10",
         className
       )}
     >
@@ -42,10 +42,10 @@ export const BentoGridItem = ({
       {header}
       <div className="group-hover/bento:translate-x-2 transition duration-200 z-10">
         {icon}
-        <div className="font-sans font-bold text- text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+        <div className="font-sans font-bold text-xl text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+        <div className="font-sans font-normal text-neutral-600 text-sm dark:text-neutral-300">
           {description}
         </div>
       </div>
